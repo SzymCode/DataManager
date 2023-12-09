c4f8c699bbb12ffe0778bb7faf6f8aa740eda539
SzymCode <s.radomski19@gmail.com>
2023-12-09 11:31:35 +0100

    Few tests for dd/dump and suffix

--- tests/Global/DumpDDTest.php
+++ tests/Global/DumpDDTest.php
@@ -1,9 +1,22 @@
 <?php
 
-describe('no dump and dd', function() {
-    test('in app')->todo();
-    test('in database')->todo();
-    test('in resources')->todo();
-    test('in routes')->todo();
-    test('in tests')->todo();
-});
+
+it('does not contain dd and dump in app')
+    ->expect('app')
+    ->not->toUse(['dd', 'dump']);
+
+it('does not contain dd and dump in database')
+    ->expect('resources')
+    ->not->toUse(['dd', 'dump']);
+
+it('does not contain dd and dump in resources')
+    ->expect('resources')
+    ->not->toUse(['dd', 'dump']);
+
+it('does not contain dd and dump in routes')
+    ->expect('routes')
+    ->not->toUse(['dd', 'dump']);
+
+it('does not contain dd and dump in tests')
+    ->expect('tests')
+    ->not->toUse(['dd', 'dump']);
