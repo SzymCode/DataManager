37f5d20b8627c23d2611c5dd5326915ea1817aad
Szymon Radomski <s.radomski19@gmail.com>
2024-02-25 01:08:15 +0100

    Maybe a little less

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -27,7 +27,7 @@ class UserSeeder extends Seeder
         ]);
 
         if (env('APP_ENV') === 'production' || 'dev') {
-            User::factory(5000)->create();
+            User::factory(2500)->create();
         }
     }
 }
