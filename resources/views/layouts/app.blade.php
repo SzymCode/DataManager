86b3492dc7097a3ea21c0113e92df43bbcad08f0
szymcode <s.radomski19@gmail.com>
2024-02-26 23:07:23 +0100

    Correction

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,29 +49,6 @@
                                 <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                             </li>
                         @endif
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }}
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                @if (Auth::user()->role === 'admin')
-                                    <a class="dropdown-item" href="{{ route('admin') }}">
-                                        Admin Panel
-                                    </a>
-                                @endif
-
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                   onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
                     </ul>
                 </div>
             </div>
