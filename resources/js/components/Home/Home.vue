1ea2e9cbc91f8e06422afecce3dadb6a03c602fe
szymcode <s.radomski19@gmail.com>
2024-03-23 21:46:49 +0100

    Installed shopify-draggable
    Implemented shopify draggable to home page

--- resources/js/components/Home/Home.vue
+++ resources/js/components/Home/Home.vue
@@ -1,72 +1,165 @@
 <template>
-    <div class="panelContainer pb-5 lg:pb-8">
+    <div class="homeBricksGrid grid mt-4 lg:mt-8 mx-auto">
         <div
-            class="homeBricksGrid grid mt-4 lg:mt-8 mx-auto col-12 justify-content-center flex"
+            class="p-3 grid-item-container BlockLayout"
+            ref="draggableContainer"
         >
-            <div class="p-3 col-12 sm:col-6 lg:col-4 grid-item-container">
-                <a href="/contacts" class="grid-item">
-                    <i class="pi pi-users" />
-                    Contacts
-                </a>
-            </div>
-            <div class="p-3 col-12 sm:col-6 lg:col-4 grid-item-container">
-                <a href="#" class="grid-item disabledItem">
-                    <i class="pi pi-comment" />
-                    Posts
-                </a>
-            </div>
-            <div class="p-3 col-12 sm:col-6 lg:col-4 grid-item-container">
-                <a href="#" class="grid-item disabledItem">
-                    <i class="pi pi-envelope" />
-                    Messages
-                </a>
-            </div>
-            <div class="p-3 col-12 sm:col-6 lg:col-4 grid-item-container">
-                <a href="#" class="grid-item disabledItem">
-                    <i class="pi pi-check-square" />
-                    Tasks
-                </a>
-            </div>
-            <div class="p-3 col-12 sm:col-6 lg:col-4 grid-item-container">
-                <a href="#" class="grid-item disabledItem">
-                    <i class="pi pi-calendar" />
-                    Calendar
-                </a>
-            </div>
-            <div class="p-3 col-12 sm:col-6 lg:col-4 grid-item-container">
-                <a href="#" class="grid-item disabledItem">
-                    <i class="pi pi-dollar" />
-                    Money
-                </a>
-            </div>
-            <div class="p-3 col-12 sm:col-6 lg:col-4 grid-item-container">
-                <a href="/activity-log" class="grid-item">
-                    <i class="pi pi-clock" />
-                    Activities
-                </a>
-            </div>
-            <div class="p-3 col-12 sm:col-6 lg:col-4 grid-item-container">
-                <a class="grid-item disabledItem">
-                    <i class="pi pi-info-circle" />
-                    Help
-                </a>
-            </div>
-            <div class="p-3 col-12 sm:col-6 lg:col-4 grid-item-container">
-                <a class="grid-item disabledItem">
-                    <i class="pi pi-cog" />
-                    Settings
-                </a>
-            </div>
+            <a
+                v-for="item in items"
+                :key="item.label"
+                :id="item.label + 'GridItem'"
+                :data-id="item.id"
+                :href="item.href"
+                :class="{
+                    disabledItem: item.disabled,
+                    'Block--is-draggable': item.draggableClass,
+                }"
+                class="grid-item"
+                @mousedown="startDragging(item)"
+                v-tooltip.top="'Hold click & move'"
+            >
+                <i :class="item.iconClass"></i>
+                {{ item.label }}
+            </a>
         </div>
     </div>
 </template>
 
-<style>
-.disabledItem:hover,
-.disabledItem:focus {
-    color: #5e5e5e;
-    box-shadow:
-        inset 0 0.5px 0 rgba(0, 0, 0, 0.15),
-        0 2px 6px rgba(0, 0, 0, 0.15);
+<script setup>
+import { ref, onMounted, onUnmounted } from 'vue'
+import { Swappable } from '@shopify/draggable'
+
+const items = ref([
+    {
+        id: 1,
+        label: 'Contacts',
+        href: '/contacts',
+        iconClass: 'pi pi-users',
+        disabled: false,
+        draggableClass: true,
+    },
+    {
+        id: 2,
+        label: 'Posts',
+        href: '#',
+        iconClass: 'pi pi-comment',
+        disabled: true,
+        draggableClass: true,
+    },
+    {
+        id: 3,
+        label: 'Messages',
+        href: '#',
+        iconClass: 'pi pi-envelope',
+        disabled: true,
+        draggableClass: true,
+    },
+    {
+        id: 4,
+        label: 'Tasks',
+        href: '#',
+        iconClass: 'pi pi-check-square',
+        disabled: true,
+        draggableClass: true,
+    },
+    {
+        id: 5,
+        label: 'Calendar',
+        href: '#',
+        iconClass: 'pi pi-calendar',
+        disabled: true,
+        draggableClass: true,
+    },
+    {
+        id: 6,
+        label: 'Money',
+        href: '#',
+        iconClass: 'pi pi-dollar',
+        disabled: true,
+        draggableClass: true,
+    },
+    {
+        id: 7,
+        label: 'Activities',
+        href: '/activity-log',
+        iconClass: 'pi pi-clock',
+        disabled: false,
+        draggableClass: true,
+    },
+    {
+        id: 8,
+        label: 'Help',
+        href: '#',
+        iconClass: 'pi pi-info-circle',
+        disabled: true,
+        draggableClass: false,
+    },
+    {
+        id: 9,
+        label: 'Settings',
+        href: '#',
+        iconClass: 'pi pi-cog',
+        disabled: true,
+        draggableClass: false,
+    },
+])
+
+let sortable
+
+onMounted(() => {
+    sortable = new Swappable(document.querySelector('.homeBricksGrid'), {
+        draggable: '.Block--is-draggable',
+        mirror: {
+            appendTo: '.homeBricksGrid',
+            constrainDimensions: true,
+        },
+        delay: 100,
+    })
+    sortable.on('drag:stop', (evt) => {
+        evt.originalSource.style.color = '#5e5e5e'
+    })
+})
+
+onUnmounted(() => {
+    if (sortable) {
+        sortable.destroy()
+    }
+})
+
+const startDragging = (item) => {
+    let color
+
+    switch (item.label) {
+        case 'Contacts':
+            color = '#1bbd79'
+            break
+        case 'Activities':
+            color = '#ffb600'
+            break
+    }
+
+    document.getElementById(item.label + 'GridItem').style.color = color
+}
+</script>
+
+<style scoped>
+.homeBricksGrid {
+    padding-left: 0;
+}
+
+.grid-item-container {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 1rem;
+}
+
+@media (min-width: 992px) {
+    .homeBricksGrid {
+        padding-left: 120px;
+    }
+}
+
+.draggable-source--is-dragging {
+    z-index: 0;
 }
 </style>
