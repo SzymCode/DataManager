6ec0bee2aa3213413602e56908700399e667413a
SzymCode <s.radomski19@gmail.com>
2024-05-05 13:04:58 +0200

    More user model tests

--- tests/Unit/Models/UserTest.php
+++ tests/Unit/Models/UserTest.php
@@ -26,8 +26,32 @@ it('can create contact from user details', function () {
     expect($contactsCount)->toBe(1);
 });
 
+it('can get isUser', function () {
+    $user = User::factory()->create(['role' => 'user']);
+
+    expect($user->isUser())->toBeTrue();
+});
+
+it('can get isTech', function () {
+    $user = User::factory()->create(['role' => 'tech']);
+
+    expect($user->isTech())->toBeTrue();
+});
+
+it('can get isTestAdmin', function () {
+    $user = User::factory()->create(['role' => 'test_admin']);
+
+    expect($user->isTestAdmin())->toBeTrue();
+});
+
 it('can get isAdmin', function () {
     $user = User::factory()->create(['role' => 'admin']);
 
     expect($user->isAdmin())->toBeTrue();
+});
+
+it('can get isSuperAdmin', function () {
+    $user = User::factory()->create(['role' => 'super_admin']);
+
+    expect($user->isSuperAdmin())->toBeTrue();
 });
