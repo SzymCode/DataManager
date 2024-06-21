c86d4616892d3358b901aafe17db00d6fec885f3
SzymCode <szymon.radomski@yahoo.com>
2024-06-21 11:40:38 +0200

    Storybook - corrections

--- storybook/src/utils/index.ts
+++ storybook/src/utils/index.ts
@@ -1,9 +1,2 @@
-/**
- *  Templates
- */
-export { default as ButtonTemplate } from './PrimeVue/templates/button'
-export { default as InputTextTemplate } from './PrimeVue/templates/inputText'
-export { default as ProgressSpinnerTemplate } from './PrimeVue/templates/progressSpinner'
-export { default as TextAreaTemplate } from './PrimeVue/templates/textArea'
-
-export { default as PrimeVueComponents } from './PrimeVue/primevueComponents'
+export { default as globalComponentsRegistry } from './globalComponentsRegistry'
+export { default as PrimeVueComponents } from './primevueComponents'
