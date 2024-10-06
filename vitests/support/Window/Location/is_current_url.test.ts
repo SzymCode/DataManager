96998cb2f8ca5d4f29a68e4d8d5f09faa3239ca2
SzymCode <szymon.radomski@yahoo.com>
2024-10-06 21:40:51 +0200

    Vitest: v2.1.2 installed + configuration & first test

---
+++ vitests/support/Window/Location/is_current_url.test.ts
@@ -0,0 +1,20 @@
+import { it, expect, beforeEach } from 'vitest'
+
+import { isCurrentUrl } from 'atomic/bosons/utils'
+
+beforeEach((): void => {
+    Object.defineProperty(window, 'location', {
+        value: {
+            pathname: '/home',
+        },
+        writable: true,
+    })
+})
+
+it('returns true when url is included in the pathname', (): void => {
+    expect(isCurrentUrl('home')).toBe(true)
+})
+
+it('returns false when url is not included in the pathname', (): void => {
+    expect(isCurrentUrl('example')).toBe(false)
+})
