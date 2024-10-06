ae41682ce3a635b285782dba58d62636fb11a28a
SzymCode <szymon.radomski@yahoo.com>
2024-10-06 19:40:51 +0200

    Corrections

--- atomic/bosons/constants/views/Home/why_us.ts
+++ atomic/bosons/constants/views/Home/why_us.ts
@@ -12,67 +12,83 @@ const createItem = (
 
 const whyUsDataArray: WhyUsDataInterface[] = [
     [
-        'pi pi-cloud',
-        'Your Own Cloud',
-        'Manage and store your data securely with your own cloud',
+        [
+            'pi pi-cloud',
+            'Your Own Cloud',
+            'Manage and store your data securely with your own cloud',
+        ],
+        [
+            'pi pi-github',
+            'Open Source',
+            'Enjoy this free template, available for anyone to use and modify',
+        ],
     ],
     [
-        'pi pi-github',
-        'Open Source',
-        'Enjoy this free template, available for anyone to use and modify',
+        [
+            'pi pi-building',
+            'Enterprise Solutions',
+            'Tailored solutions for large-scale enterprises to optimize operations',
+        ],
+        [
+            'pi pi-share-alt',
+            'Share Data',
+            'Efficiently share your data with your co-workers',
+        ],
     ],
     [
-        'pi pi-building',
-        'Enterprise Solutions',
-        'Tailored solutions for large-scale enterprises to optimize operations',
+        [
+            'pi pi-chart-line',
+            'Analytics',
+            'Gain insights and make data-driven decisions with powerful analytics',
+        ],
+        [
+            'pi pi-palette',
+            'Customizable',
+            'Customize this template to create a unique look and feel',
+        ],
     ],
     [
-        'pi pi-share-alt',
-        'Share Data',
-        'Efficiently share your data with your co-workers',
+        [
+            'pi pi-file-pdf',
+            'Many Extensions',
+            'Streamline document handling and storage in your work',
+        ],
+        [
+            'pi pi-database',
+            'Database Services',
+            'Secure and reliable database management services for your data',
+        ],
     ],
     [
-        'pi pi-chart-line',
-        'Analytics',
-        'Gain insights and make data-driven decisions with powerful analytics',
+        [
+            'pi pi-history',
+            'Stay up to date',
+            'Track and manage activities efficiently',
+        ],
+        [
+            'pi pi-sync',
+            'Synchronization',
+            'Keep your data synchronized across devices and platforms',
+        ],
     ],
     [
-        'pi pi-palette',
-        'Customizable',
-        'Customize this template to create a unique look and feel',
-    ],
-    [
-        'pi pi-file-pdf',
-        'Many Extensions',
-        'Streamline document handling and storage in your work',
-    ],
-    [
-        'pi pi-database',
-        'Database Services',
-        'Secure and reliable database management services for your data',
-    ],
-    [
-        'pi pi-history',
-        'Stay up to date',
-        'Track and manage activities efficiently',
-    ],
-    [
-        'pi pi-sync',
-        'Synchronization',
-        'Keep your data synchronized across devices and platforms',
-    ],
-    [
-        'pi pi-shield',
-        'Security',
-        'Advanced security solutions to protect your data and systems',
-    ],
-    [
-        'pi pi-bell',
-        'Notifications',
-        'Real-time notifications to keep you informed of critical events',
+        [
+            'pi pi-shield',
+            'Security',
+            'Advanced security solutions to protect your data and systems',
+        ],
+        [
+            'pi pi-bell',
+            'Notifications',
+            'Real-time notifications to keep you informed of critical events',
+        ],
     ],
 ]
 
 export const whyUsData: WhyUsDataInterface[] = whyUsDataArray.map(
-    ([icon, title, description]) => createItem(icon, title, description)
+    (group: WhyUsDataInterface) => ({
+        items: group.map(([icon, title, description]) =>
+            createItem(icon, title, description)
+        ),
+    })
 )
--- atomic/organisms/OverlayPanel.vue
+++ atomic/organisms/OverlayPanel.vue
@@ -1,33 +1,17 @@
 <template>
     <ad-button
         @click="toggle"
+        :icon="
+            props.overlayPanelClass.includes('terminal')
+                ? 'pi pi-code'
+                : props.icon
+        "
         :src="props.src"
-        :class="buttonClass"
-        :style="buttonStyle"
+        :class="props.buttonClass"
+        :style="props.buttonStyle"
         type="main"
         rounded
-    >
-        <svg
-            v-if="!props.src"
-            version="1.0"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 225.000000 225.000000"
-            preserveAspectRatio="xMidYMid meet"
-        >
-            <g
-                transform="translate(0.000000,225.000000) scale(0.100000,-0.100000)"
-                fill="#000000"
-                stroke="none"
-            >
-                <path
-                    d="M49 1811 c-27 -26 -49 -56 -49 -67 0 -11 113 -132 275 -294 l275 -275 -275 -275 c-151 -151 -275 -282 -275 -290 0 -23 92 -110 115 -110 28 0 675 648 675 675 0 23 -657 685 -679 685 -8 0 -36 -22 -62 -49z"
-                />
-                <path
-                    d="M878 559 c-14 -8 -18 -24 -18 -77 0 -40 5 -73 12 -80 9 -9 174 -12 679 -12 594 0 669 2 683 16 11 10 16 34 16 74 0 40 -5 64 -16 74 -14 14 -89 16 -677 16 -431 -1 -668 -4 -679 -11z"
-                />
-            </g>
-        </svg>
-    </ad-button>
+    />
 
     <OverlayPanel
         ref="op"
@@ -41,7 +25,7 @@
         :pt-options="props.ptOptions"
         :unstyled="props.unstyled"
         :closeOnEscape="props.closeOnEscape"
-        :class="overlayPanelClass"
+        :class="props.overlayPanelClass"
     >
         <slot />
     </OverlayPanel>
