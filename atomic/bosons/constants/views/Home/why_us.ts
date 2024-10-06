15c65da5ca47457720a26fd3a0cbd9525115e15a
SzymCode <szymon.radomski@yahoo.com>
2024-10-06 20:10:30 +0200

    Corrections

--- atomic/bosons/constants/views/Home/why_us.ts
+++ atomic/bosons/constants/views/Home/why_us.ts
@@ -1,94 +1,90 @@
 import { WhyUsDataInterface } from 'atomic/bosons/types'
 
 const createItem = (
-    icon?: string,
-    title?: string,
-    description?: string
-): WhyUsDataInterface => ({
-    icon,
-    title,
-    description,
-})
+    icon: string,
+    title: string,
+    description: string
+): WhyUsDataInterface =>
+    ({
+        icon,
+        title,
+        description,
+    }) as const
 
-const whyUsDataArray: WhyUsDataInterface[] = [
+const itemsData: WhyUsDataInterface[] = [
     [
-        [
-            'pi pi-cloud',
-            'Your Own Cloud',
-            'Manage and store your data securely with your own cloud',
-        ],
-        [
-            'pi pi-github',
-            'Open Source',
-            'Enjoy this free template, available for anyone to use and modify',
-        ],
+        'pi pi-cloud',
+        'Your Own Cloud',
+        'Manage and store your data securely with your own cloud',
     ],
     [
-        [
-            'pi pi-building',
-            'Enterprise Solutions',
-            'Tailored solutions for large-scale enterprises to optimize operations',
-        ],
-        [
-            'pi pi-share-alt',
-            'Share Data',
-            'Efficiently share your data with your co-workers',
-        ],
+        'pi pi-github',
+        'Open Source',
+        'Enjoy this free template, available for anyone to use and modify',
     ],
     [
-        [
-            'pi pi-chart-line',
-            'Analytics',
-            'Gain insights and make data-driven decisions with powerful analytics',
-        ],
-        [
-            'pi pi-palette',
-            'Customizable',
-            'Customize this template to create a unique look and feel',
-        ],
+        'pi pi-building',
+        'Enterprise Solutions',
+        'Tailored solutions for large-scale enterprises to optimize operations',
     ],
     [
-        [
-            'pi pi-file-pdf',
-            'Many Extensions',
-            'Streamline document handling and storage in your work',
-        ],
-        [
-            'pi pi-database',
-            'Database Services',
-            'Secure and reliable database management services for your data',
-        ],
+        'pi pi-share-alt',
+        'Share Data',
+        'Efficiently share your data with your co-workers',
     ],
     [
-        [
-            'pi pi-history',
-            'Stay up to date',
-            'Track and manage activities efficiently',
-        ],
-        [
-            'pi pi-sync',
-            'Synchronization',
-            'Keep your data synchronized across devices and platforms',
-        ],
+        'pi pi-chart-line',
+        'Analytics',
+        'Gain insights and make data-driven decisions with powerful analytics',
     ],
     [
-        [
-            'pi pi-shield',
-            'Security',
-            'Advanced security solutions to protect your data and systems',
-        ],
-        [
-            'pi pi-bell',
-            'Notifications',
-            'Real-time notifications to keep you informed of critical events',
-        ],
+        'pi pi-palette',
+        'Customizable',
+        'Customize this template to create a unique look and feel',
     ],
-]
+    [
+        'pi pi-file-pdf',
+        'Many Extensions',
+        'Streamline document handling and storage in your work',
+    ],
+    [
+        'pi pi-database',
+        'Database Services',
+        'Secure and reliable database management services for your data',
+    ],
+    [
+        'pi pi-history',
+        'Stay up to date',
+        'Track and manage activities efficiently',
+    ],
+    [
+        'pi pi-sync',
+        'Synchronization',
+        'Keep your data synchronized across devices and platforms',
+    ],
+    [
+        'pi pi-shield',
+        'Security',
+        'Advanced security solutions to protect your data and systems',
+    ],
+    [
+        'pi pi-bell',
+        'Notifications',
+        'Real-time notifications to keep you informed of critical events',
+    ],
+] as const
 
-export const whyUsData: WhyUsDataInterface[] = whyUsDataArray.map(
-    (group: WhyUsDataInterface) => ({
-        items: group.map(([icon, title, description]) =>
-            createItem(icon, title, description)
-        ),
-    })
+export const whyUsData: { items: WhyUsDataInterface[] }[] = itemsData.reduce(
+    (result, item, index): { items: WhyUsDataInterface[] }[] => {
+        if (index % 2 === 0) {
+            result.push({
+                items: [
+                    createItem(...item),
+                    createItem(...itemsData[index + 1]),
+                ],
+            })
+        }
+        return result
+    },
+    [] as { items: WhyUsDataInterface[] }[]
 )
