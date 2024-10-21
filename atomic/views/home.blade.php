9d31143e812bbd8349538acf9a18497216b5f2ea
SzymCode <szymon.radomski@yahoo.com>
2024-10-21 19:02:35 +0200

    Performance: Preload for LCP - home page

--- atomic/views/home.blade.php
+++ atomic/views/home.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
+    <link rel="preload" fetchpriority="high" as="image" href="charts.svg" type="image/svg+xml">
+
     <div id="home">
         <home></home>
         <screen-loader />
